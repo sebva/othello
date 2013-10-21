b423cf75be3ebf91f60d4c317fa1b1dc19b0d10c
Sébastien Vaucher <seb.vaucher@gmail.com>
2013-10-21 23:21:01 +0200

    Implement Othello game logic + dumb AI algorithm

---
+++ src/Participants/FortVaucher/Board.java
@@ -0,0 +1,150 @@
+package Participants.FortVaucher;
+
+
+import Othello.Move;
+
+/**
+ * This class represents the board of the game.
+ * This class has been thoroughly tested !
+ *
+ * @author Sébastien Vaucher
+ */
+public class Board
+{
+	/**
+	 * The board. Be careful with the order !
+	 *
+	 * /!\  /!\ /!\ /!\  /!\
+	 * /!\ [Column][Row] /!\
+	 * /!\  /!\ /!\ /!\  /!\
+	 */
+	private Color[][] board;
+	private final Color ourColor;
+	private final Color theirColor;
+
+	// Constants
+	public final int nbBoxes;
+
+	public enum Color
+	{
+		RED, BLUE, NOTHING
+	}
+
+	public Board(Color ourColor, int nbBoxes)
+	{
+		board = new Color[nbBoxes][nbBoxes];
+		this.ourColor = ourColor;
+		this.theirColor = ourColor == Color.BLUE ? Color.RED : Color.BLUE;
+		this.nbBoxes = nbBoxes;
+
+		// Board initialization
+		for (int i = 0; i < nbBoxes; i++)
+			for (int j = 0; j < nbBoxes; j++)
+				board[i][j] = Color.NOTHING;
+
+		// Adding the 4 existing bubbles at the center
+		board[nbBoxes / 2][nbBoxes / 2] = Color.BLUE;
+		board[nbBoxes / 2 - 1][nbBoxes / 2 - 1] = Color.BLUE;
+		board[nbBoxes / 2 - 1][nbBoxes / 2] = Color.RED;
+		board[nbBoxes / 2][nbBoxes / 2 - 1] = Color.RED;
+	}
+
+	/**
+	 * Can be used to print the current state of the board to the standard system output
+	 */
+	public void printBoard()
+	{
+		for (int i = 0; i < nbBoxes; i++)
+		{
+			System.out.println();
+			for (int j = 0; j < nbBoxes; j++)
+				System.out.print(board[j][i] + "\t");
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Apply a move to the board
+	 * @param move The move to apply
+	 * @param fromOurselves true = The player who realized this move is ourselves
+	 */
+	public void applyMove(Move move, boolean fromOurselves)
+	{
+		// The color to which the bubbles will be moved to
+		Color flipColor = fromOurselves ? ourColor : theirColor;
+		// The current color of the bubbles
+		Color otherColor = fromOurselves ? theirColor : ourColor;
+
+		// Flip the move bubble
+		board[move.i][move.j] = flipColor;
+
+		// Chain reaction in all 8 directions
+		for(int i = -1; i <= 1; i++)
+			for(int j = -1; j <= 1; j++)
+				if(!(i == 0 && j == 0))
+				{
+					// If there is something to flip in the current direction
+					if(numberOfBoxesFlippedInDirection(move, i, j, otherColor) > 0)
+					{
+						int col = move.i, row = move.j;
+
+						while(board[col + i][row + j] == otherColor)
+						{
+							col += i;
+							row += j;
+							board[col][row] = flipColor;
+						}
+					}
+				}
+	}
+
+	/**
+	 * Returns the number of bubbles which would be flipped if someone would play on this box
+	 * @param move The coordinates of the box
+	 * @param fromOurselves true = The move would be played by us
+	 * @return A number of bubbles which would then belong to the player playing
+	 */
+	public int numberOfBoxesFlipped(Move move, boolean fromOurselves)
+	{
+		// Don't attempt to play where there already is a bubble
+		if(board[move.i][move.j] != Color.NOTHING)
+			return 0;
+
+		int amount = 0;
+		Color flipColor = fromOurselves ? theirColor : ourColor;
+
+		// Check in all 8 directions
+		for(int i = -1; i <= 1; i++)
+			for(int j = -1; j <= 1; j++)
+				if(i != 0 || j != 0)
+					amount += numberOfBoxesFlippedInDirection(move, i, j, flipColor);
+
+		return amount;
+	}
+
+	private int numberOfBoxesFlippedInDirection(Move move, int colIncrement, int rowIncrement, Color flipColor)
+	{
+		int col = move.i, row = move.j;
+		int amount = 0;
+
+		while(true)
+		{
+			col += colIncrement;
+			row += rowIncrement;
+
+			// Bounds check
+			if(row < 0 || row >= nbBoxes || col < 0 || col >= nbBoxes)
+				return 0;
+
+			if(board[col][row] == flipColor)
+				amount++;
+			else
+				break;
+		}
+
+		if(board[col][row] == Color.NOTHING)
+			return 0;
+		else
+			return amount;
+	}
+}
--- src/Participants/FortVaucher/Joueur.java
+++ src/Participants/FortVaucher/Joueur.java
@@ -1,54 +1,67 @@
-/* 
-Exemple d'implémentation d'un joueur d'Othello. Cette implémentation sert uniquement
-à démontrer le principe, mais n'implémente aucune intelligence: les coups à jouer sont 
-simplement lus à la console!
-*/
-
-// Votre version sera dans Participants.<VosNoms>
 package Participants.FortVaucher;
-
-// Pour l'interopérabilité: il faut une représentation commune des coups!
 
 import Othello.Move;
 
-import java.util.Scanner;
+/**
+ * @author Sébastien Vaucher
+ */
+public class Joueur extends Othello.Joueur
+{
 
-// Utile seulement dans cet exemple, pour lire l'entrée de l'utilisateur à la console
+	private Board board;
 
-// Vous devrez étendre Othello.Joueur pour implémenter votre propre joueur...
-public class Joueur extends Othello.Joueur {
+	/**
+	 * @param depth Alpha-beta algorithm depth
+	 * @param playerID 0 = Red, 1 = Blue
+	 */
+	public Joueur(int depth, int playerID)
+	{
+		super(depth, playerID);
 
-	// depth: profondeur alpha-beta
-	// playerID: 0 = rouge, 1 = bleu
-	public Joueur(int depth, int playerID) {
-		super();
-
-
+		board = new Board(playerID == 0 ? Board.Color.RED : Board.Color.BLUE, super.size);
 	}
-	
-	Scanner stdin = new Scanner(System.in);
 
-	// Méthode appelée à chaque fois que vous devez jouer un coup.
-	// move est le coup joué par l'adversaire
-	public Move nextPlay(Move move) {
-		// Ici, vous devrez
-		// - Mettre à jour votre représentation du jeu en fonction du coup joué par l'adversaire
-		// - Décider quel coup jouer (alpha-beta!!)
-		// - Remettre à jour votre représentation du jeu
-		// - Retourner le coup choisi
-		// Mais ici, on se contente de lire à la console:
-		Move result = null;
-		if (move != null) 
-			System.out.println("Coup adverse: " + move.i + ", " + move.j);
-		System.out.println("Votre coup: ");
-		System.out.print("Colonne (-1 si aucun coup possible): ");
-		int i = stdin.nextInt();
-		if (i != -1) {
-			System.out.print("Ligne: ");
-			int j =  stdin.nextInt();
-			result = new Move(i,j);
-		}
-		return result;
+	/**
+	 * This method is called whenever we have to play
+	 * @param move The opponent's move, which has just happened
+	 * @return Our next move
+	 */
+	@Override
+	public Move nextPlay(Move move)
+	{
+		// Apply the opponent's move to the board
+		if(move != null)
+			board.applyMove(move, false);
+
+
+
+		// BEGIN Temp dumb algorithm
+		Move bestMove = null;
+		int bestAmount = -1;
+		Move currentMove = new Move();
+		for(int i = 0; i < super.size; i++)
+			for(int j = 0; j < super.size; j++)
+			{
+				currentMove.i = i;
+				currentMove.j = j;
+
+				int amount = board.numberOfBoxesFlipped(currentMove, true);
+				if(amount > bestAmount)
+				{
+					bestAmount = amount;
+					bestMove = new Move(i, j);
+				}
+			}
+
+		if(bestAmount <= 0)
+			// This method has to return null when it's not possible to play
+			return null;
+		// END Temp dumb algorithm
+
+		// Apply our move to the board
+		board.applyMove(bestMove, true);
+
+		return bestMove;
 	}
 
 }
