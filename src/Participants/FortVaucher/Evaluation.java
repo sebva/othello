f536e81f5136dccaa549d942ebd3d5b419943f42
root <root@Lapscott.(none)>
2013-10-28 16:21:30 +0100

    Updated eval function (subs. table)

--- src/Participants/FortVaucher/Evaluation.java
+++ src/Participants/FortVaucher/Evaluation.java
@@ -9,20 +9,29 @@ public class Evaluation
 {
 	private static final int[][] boardWeights =
 		{
-		{120, -20, 20,  5,  5, 20, -20, 120},
+/*		{120, -20, 20,  5,  5, 20, -20, 120},
 		{-20, -40, -5, -5, -5, -5, -40, -20},
 		{ 20,  -5, 15,  3,  3, 15,  -5,  20},
 		{  5,  -5,  3,  3,  3,  3,  -5,   5},
 		{  5,  -5,  3,  3,  3,  3,  -5,   5},
 		{ 20,  -5, 15,  3,  3, 15,  -5,  20},
 		{-20, -40, -5, -5, -5, -5, -40, -20},
-		{120, -20, 20,  5,  5, 20, -20, 120}
+		{120, -20, 20,  5,  5, 20, -20, 120}*/
+			{500, -150, 30, 10, 10, 30, -150, 500},
+			{-150, -250, 0, 0, 0, 0, -250, -150},
+			{30, 0, 1, 2, 2, 1, 0, 30},
+			{10, 0, 2, 16, 16, 2, 0, 10},
+			{10, 0, 2, 16, 16, 2, 0, 10},
+			{30, 0, 1, 2, 2, 1, 0, 30},
+			{-150, -250, 0, 0, 0, 0, -250, -150},
+			{500, -150, 30, 10, 10, 30, -150, 500}
 		};
 
 
 	public static double evaluateBoard(byte[][] board, byte evaluatedPlayerColor)
 	{
 		int evaluation = 0;
+		int totalPieces = 0;
 		for (int i=0; i < 8; i++)
 		{
 			for (int j=0; j < 8; j++)
@@ -32,10 +41,14 @@ public class Evaluation
 			   	{
 				   	int add = boardWeights[j][i];
 					add *= (piece == evaluatedPlayerColor ? 1 : -1);
+
+					totalPieces += (piece == evaluatedPlayerColor ? 1 : -1);
+
 				   	evaluation += add;
 			   	}
 			}
 		}
-		return evaluation;
+
+		return evaluation + 0*totalPieces;
 	}
 }
